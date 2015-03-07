cc7a3bf22e98fc98ac1b5e27bd76ad24fc4301c3
James Wu <jiwu@cs.washington.edu>
2015-03-06 18:35:42 -0800

    event code thres detection functional

--- AdroitBCI_imgtraining.m
+++ AdroitBCI_imgtraining.m
@@ -1,3 +1,12 @@
+%% Init
+% variables
+clear all
+rootpath = pwd();
+[adroitpath, port, timeout, read_timeout, synergydims, originpos, protosynergies]...
+    = setup_bci(rootpath);
+[model, act, gain_A, gain_W, gain_F, vizIP, vizDir, so, m, gainP, gainD]...
+    = setup_adroit(adroitpath, originpos);
+
 filename = 'buffer://localhost:1972';
 
 % read the header for the first time to determine number of channels and sampling rate
@@ -6,7 +15,7 @@ hdr = ft_read_header(filename);
 blocksize  = 0.100*hdr.Fs;
 chanindx   = 1:hdr.nChans;
 eventsize = 0;
-intrial = 0;
+intrial = logical(0);
 
 while true
   % determine number of samples available in buffer
@@ -17,7 +26,7 @@ while true
     % dat = ft_read_data(filename, 'header', hdr, 'begsample', begsample, 'endsample', endsample, 'chanindx', chanindx);
     eventcode = ft_read_event(filename, 'header', hdr);
     currentsize = size({eventcode.value}, 2);
-	if(currentsize > eventsize)
+	if(currentsize > eventsize) %% this needs to be a function
         disp('new events!')
         disp([eventcode.type])
         newstim = [eventcode(eventsize+find(strcmp('StimulusCode', {eventcode((eventsize+1):currentsize).type}))).value];
@@ -25,9 +34,17 @@ while true
         eventsize = currentsize;
     end
     
-%     if(newstim > 0)
-%         intrial = 1;
-%         switch(newstim)
+    if(newstim > 0 && ~intrial)
+        intrial = 1;
+        switch(mod(newstim,4))
+            case 1
+                
+            case 2
+            case 3
+        end
+    elseif(newstim == 0 && intrial)
+        intrial = 0;
+    end
     
 %     if(eventcode(1, 3).value) == 1
 %         while(eventcode(1, 3).value > 0)
--- setup_bci.m
+++ setup_bci.m
@@ -2,8 +2,8 @@ function [ adroitpath, port, timeout, read_timeout, synergydims, originpos, prot
     = setup_bci( rootpath )
 %SETUP_BCI Setup all the AdroitBCI environment
 % Sets up the variables and the paths needed for AdroitBCI
-adroitpath      = 'C:\Users\jiwu\Dropbox\BCI_Hand\ApplicationModules\Adroit_sim(0.72)\'; % Vikash's Adroit mojoco install
-port            = 15998;
+adroitpath      = 'C:\Users\jiwu\Dropbox\BCI_Hand\Contribs\Adroit_sim(0.72)\'; % Vikash's Adroit mojoco install
+fieldtrippath   = 'C:\Users\jiwu\Dropbox\BCI_Hand\Contribs\fieldtrip\';
 timeout         = 3000; % How long to wait for a BCI2000 connection
 read_timeout    = 500/1000; % How long to wait for data lagout during connection
 synergydims     = 2;
@@ -13,9 +13,13 @@ addpath([rootpath '/bci_depends/'])
 addpath([rootpath '/bci_depends/stoploop/'])
 addpath(adroitpath)
 addpath([adroitpath '/VizualizerComm/']) % Vikash's network C visualizer
-javaaddpath([rootpath '/bci_depends/tcp_ip_socket_comms_java/']) % fast entire-buffer reader
+addpath([fieldtrippath '/fileio/']_)
 
 originpos = csvread('bci_depends/Toronto_originpos.csv');
 protosynergies = csvread('bci_depends/Toronto_synergies.csv');
+if(synergydims < size(protosynergies, 2)) % if need to truncate synergies\
+    warning(['Warning: Truncating to ' synergydims ' synergies']);
+    protosynergies = protosynergies(:, 1:synergydims);
+end
 
 end
